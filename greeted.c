760db978ae3423c57908637fe7e877ce3768c0e6
kops1995 <38175822+kops1995@users.noreply.github.com>
2019-06-26 14:27:33 +0900

    소개 및 질문
    이곳은 ~, 무슨일로 ~ 추가

--- greeted.c
+++ greeted.c
@@ -6,10 +6,13 @@ int main()
 	
 
 	
-	printf("안녕하세요");
 	printf("Hello\n");
 	printf("Bye\n");
 	printf("Hi\n");
+	printf("안녕하세요\n");
+	printf("안녕히가세요\n");
+	printf("이곳은 건우글로벌입니다.\n");
+	printf("무슨일로 오셨나요?\
 
 
 	return 0;
